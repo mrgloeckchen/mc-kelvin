43540b2c7bb3d8264910bf4538462e84aea9b1ca
mrgloeckchen <dkuelahli@icloud.com>
2025-11-05 19:57:26 +0100

    Delete Kelvin_BP directory

--- Kelvin_BP/scripts/behaviour/stay.js
+++
@@ -1,9 +0,0 @@
-import { getKelvinEntity, getState } from "./utils.js";
-
-export function stayTick() {
-  const kelvin = getKelvinEntity();
-  if (!kelvin) return;
-  const data = getState().taskData;
-  if (!data?.location) return;
-  kelvin.teleport(data.location, { keepVelocity: false });
-}
